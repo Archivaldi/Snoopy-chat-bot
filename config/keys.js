41d47d5a96405bb19d29252b4af1be87c5f792f7
Artur Markov <artur.markov1860@gmail.com>
2019-11-12 16:59:51 -0800

    see

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,10 @@
 if (process.env.NODE_ENV === "production"){
     module.exports = require("./prod");
     console.log(process.env.GOOGLE_PRIVATE_KEY)
+    console.log(process.env.GOOGLE_PROJECT_ID)
+    console.log(process.env.DIALOGFLOW_SESSION_ID)
+    console.log(process.env.DIALOGFLOW_LANGUAGE_CODE)
+    console.log(process.env.GOOGLE_CLIENT_EMAIL)
 } else {
     module.exports = require("./dev");
 }
