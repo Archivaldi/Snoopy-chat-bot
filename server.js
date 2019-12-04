225a31d06193c109726227f9f7e4d967830c5869
Artur Markov <artur.markov1860@gmail.com>
2019-12-03 18:43:26 -0800

    path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors")
 const app = express();
+const path = require('path')
 const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
