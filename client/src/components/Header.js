32a7ca867ceb0b054ac4756476a3dcd23cc6fdd5
Irina K <irtiola@gmail.com>
2019-12-04 20:02:21 -0800

    some changes with css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import './App.css';
 
 const Header = () =>
     (
 
-        <nav style={{backgroundColor: "transparent"}}>
+        <nav class="header">
             <div className="nav-wrapper" id="header" style={{marginBottom:"30px",fontStyle:"Italic",border:"none !important"}}>
-                <Link to={"/"} className="brand-logo" style={{fontStyle:"italic"}}>Snoopy.Clothing 👕</Link>
+                <Link to={"/"} className="brand-logo" style={{fontStyle:"italic"}}>Snoopy Chat </Link>
             </div>
         </nav>
     )
--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,39 +1,15 @@
 
 import React from 'react';
-import { Fade } from 'react-slideshow-image';
+import { Image } from "react-image-and-background-image-fade";
 import './landing.css';
 
-const fadeImages = [
-    
-    "https://cdn.pixabay.com/photo/2015/01/21/13/21/sale-606687__340.png",
-    "https://i.ytimg.com/vi/9ZMw6dGRkGo/maxresdefault.jpg",
-   
-];
-
-const fadeProperties = {
-  duration: 3000,
-  transitionDuration: 300,
-  infinite: true,
-  indicators: true
-}
 
 const Landing = () => {
-  return (
-    <Fade {...fadeProperties}>
-      <div className="each-fade">
-        <div className="image-container">
-          <img src={fadeImages[0]} />
-          
-        </div>
-        
+  return ( 
+    <div class = "bounce">
+      <p> Welcome.Добро пожаловать.Bienvenido.Accueil </p> 
       </div>
-      <div className="each-fade">
-        <div className="image-container">
-          <img src={fadeImages[1]} />
-        </div>
-      </div>
-      
-    </Fade>
+
   )
 }
 
