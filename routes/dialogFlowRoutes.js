5e22733103640762c4a4d64365305ed02c079090
Artur Markov <artur.markov1860@gmail.com>
2020-02-19 14:06:40 -0800

    fixed bug

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -14,20 +14,20 @@ module.exports = app => {
     })
 
     app.post('/getItemData', (req,res) => {
-      const item = req.body.item
+      const item = req.body.item;
       var results = [];
-      let url = "https://www.ebay.com/sch/i.html?_nkw="+item+"+man+women&_sop=15&rt=nc&LH_ItemCondition=1000"
+      let url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313.TR12.TRC2.A0.H0.Xhats.TRS0&_nkw="+item+"&_sacat=0"
           request.get(url, function (err, response, body) {
             var $ = cheerio.load(body);
             $("li.s-item").each(function (i, element) {
-              var a = $(element).children().eq(0).children().eq(1).children().eq(1).attr("href");
+              var a = $(element).children().eq(0).children().eq(0).children().eq(0).children().eq(0).attr("href");
               var img = $(element).children().eq(0).children().eq(0).children().eq(0).children().eq(0).children().eq(0).children().eq(1).attr("src");
-              var title = $(element).children().eq(0).children().eq(1).children().eq(1).children().eq(1).text();
+              var title = $(element).children().eq(0).children().eq(1).children().eq(1).children().eq(0).text();
               var price= $(element).children().eq(0).children().eq(1).children().eq(3).children().eq(0).children().eq(0).text();
               var item = {a, img, title, price}
                 results.push(item)
             });
-
+            console.log(results);
             res.send(results)
         });
 
